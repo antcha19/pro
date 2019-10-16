bec2f74eb1f542edcdef7945c1f5e46541609bc2
antcha19 <55527269+antcha19@users.noreply.github.com>
2019-10-16 18:50:33 +0200

    actualizado 16202019

---
+++ boletin4/src/boletin4/ejercicio31.java
@@ -0,0 +1,29 @@
+/*
+ * Antonio yepez 16102019
+ * Escribe un programa que lea un número entero de 3 cifras y
+ * 
+ *  muestre por separado las cifras del número. Un ejemplo de ejecución sería
+ */
+
+package boletin4;
+
+import java.util.Scanner;
+
+public class ejercicio31 {
+
+	public static void main(String[] args) {
+		
+		//variable scanner
+		Scanner teclado=new Scanner(System.in);
+				
+		int x;
+				
+		System.out.println("escribre los numeros");
+		x = teclado.nextInt();
+		
+		
+	
+
+	}
+
+}
