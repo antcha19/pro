a98dcff80427a93e3d0e41dd4732696eef344e94
antcha19 <antcha@alu.iesserpis.org>
2019-12-03 11:36:48 +0100

    74

---
+++ unidad6/src/unidad6/ejercicio74.java
@@ -0,0 +1,65 @@
+/*
+ * Antonio yepez  03122019
+ * Hacer una función que, dado un String, imprima dicha cadena en una caja de caract*/
+package unidad6;
+
+import java.util.*;
+
+public class ejercicio74 {
+
+	public static void main(String[] args) {
+
+		String frase;
+		int contador = 0;
+		Scanner teclado = new Scanner(System.in);
+
+		System.out.println("iNTRODUZCA NOMBRE");
+		frase = teclado.nextLine();  //  escribir una frase  quue los espacios no los reconozca
+		caja(frase);
+
+		
+	}
+
+	public static void caja(String frase) {
+
+		// imprimi la primera linea
+				for (int i = 0; i < frase.length() + 2; i++) {
+					System.out.print("*");
+
+				}
+				System.out.println("");
+
+				// segunda linea
+				for (int i = 0; i < frase.length() + 2; i++) {
+					if (i == 0) {
+						System.out.print("*");
+					} else if (i == frase.length() + 1) {
+						System.out.println("*");
+					} else {
+						System.out.print(" ");
+					}
+
+				}
+
+				/// imrpime la frase
+				System.out.println("*" + frase + "*");
+				
+				// imprime linea 4
+				for (int i = 0; i < frase.length() + 2; i++) {
+					if (i == 0) {
+						System.out.print("*");
+					} else if (i == frase.length() + 1) {
+						System.out.println("*");
+					} else {
+						System.out.print(" ");
+					}
+
+				}
+
+				// imprime la ultima linea
+				for (int j = 0; j < frase.length() + 2; j++) {
+					System.out.print("*");
+				}
+	}
+
+}
---
+++ unidad6/src/unidad6/ejercicio75.java
@@ -0,0 +1,25 @@
+package unidad6;
+
+
+import java.util.*;
+public class ejercicio75 {
+
+	public static void main(String[] args) {
+		 Scanner teclado = new Scanner (System.in);
+		 
+		 int numero=0;
+
+		 System.out.println("introduzca numero a convertir");
+		 numero = teclado.nextInt();
+		 
+		 
+	}
+
+	
+	public String convierte(int n) {
+		String micadena="";
+		
+		
+		return micadena;
+	}
+}
