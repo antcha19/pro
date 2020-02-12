34c5c85edaeefc63cc7cf90650df0f3e3cdaa1d5
antcha19 <antcha@alu.iesserpis.org>
2020-02-12 12:34:30 +0100

    airways

---
+++ POO/src/pacoairways/airwyas.java
@@ -0,0 +1,29 @@
+package pacoairways;
+
+import java.util.ArrayList;
+
+public class airwyas {
+
+	// atributos
+	private int id;
+	private String origen;
+	private String destino;
+	private String ida;
+	private String vuelta;
+
+	public airwyas(int id, String origen, String destino, String ida, String vuelta) {
+		this.id = id;
+		this.origen = origen;
+		this.destino = destino;
+		this.ida = ida;
+		this.vuelta = vuelta;
+
+		System.out.println("billete comprado");
+
+	}
+
+	void todos() {
+		System.out.println(id+origen+destino+ida+vuelta);
+	}
+
+}
---
+++ POO/src/pacoairways/principalairways.java
@@ -0,0 +1,82 @@
+package pacoairways;
+
+import java.util.*;
+
+public class principalairways {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner teclado = new Scanner(System.in);
+		ArrayList<airwyas> listavuelos = new ArrayList<airwyas>();
+		
+		boolean salir = false;
+		String billete = "";
+		int id = 1;
+		int opcion;
+		int cancelar;
+		char estado;
+
+		while (!salir) {
+
+			System.out.println("1.comprar billete de avion");
+			System.out.println("2.cancelar billete de avion");
+			System.out.println("3.estado de mi reserva");
+			System.out.println("s.salir");
+
+			System.out.println("Escribe una de las opciones");
+			opcion = teclado.nextInt();
+			teclado.nextLine();
+
+			switch (opcion) {
+			case 1:
+				System.out.println("introduzca origen, destino, dia de ida y dia de vuelta separados por @:");
+
+				billete = teclado.nextLine();
+
+				String[] parte = billete.split("@");
+				String origen = parte[0];
+				String destino = parte[1];
+				String ida = parte[2];
+				String vuelta = parte[3];
+				// creo y lo guardo
+			
+				airwyas vuelos = new airwyas(id, origen, destino, ida, vuelta);
+				
+			//	vuelos = listavuelos ;
+				id++;
+				listavuelos.add(vuelos);
+
+				break;
+			case 2:
+				System.out.println("¿introduzca el id del billete a cancelar:?");
+				cancelar = teclado.nextInt();
+				cancelar = id;
+
+				listavuelos.remove(id);
+
+				break;
+			case 3:
+				
+				System.out.println("que vuelos desa ver? (V-vigentes)");
+				estado = teclado.next().charAt(0);
+				if (estado == 't' || estado == 'T') {
+					Iterator iter = listavuelos.iterator();
+					while(iter.hasNext()) {
+				//		aux = (listavuelos).iter.next();
+					}
+				}
+				break;
+
+			case 9:
+				salir = true;
+				break;
+			default:
+				System.out.println("Solo números entre 1 y 5 . Pulsa el 9 para salir");
+			}
+
+		}
+
+	}
+
+}
